f102e5e14cea3523ed69739da4468c0060386300
Smit <smit4180@gmail.com>
2023-07-21 17:13:58 +0530

    First Directory

--- app/src/main/java/com/example/tictactoe2/Splash.java
+++ app/src/main/java/com/example/tictactoe2/Splash.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
@@ -17,6 +18,10 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        //getSupportActionBar().hide();
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
 
 
         ImageView mew = findViewById(R.id.imageViewme);
